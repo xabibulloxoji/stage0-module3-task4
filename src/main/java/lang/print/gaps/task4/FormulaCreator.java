e64d9ecc3d131cf393a02be59748f5aa418b1bc0
Muhammadsodiq <muhammadsodiqxabibulloxoji@gmail.com>
2022-08-02 12:28:21 +0500

    done

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a = 10;
+        int b = 4;
+        System.out.println(((9 * (a * a)) - (5 * b) + 2 + a - 7) * ((a + b - (4 * a * b)) / 2));
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,13 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 99;
+        System.out.println((char) c);
+        int h = 102;
+        System.out.println((char) h);
+        int a = 97;
+        System.out.println((char) a);
+        int r = 114;
+        System.out.println((char) r);
     }
 }
