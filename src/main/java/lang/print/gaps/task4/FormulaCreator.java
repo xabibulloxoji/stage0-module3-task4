fa3714b304d734c9ba73227b7d77569390e4f535
Muhammadsodiq <muhammadsodiqxabibulloxoji@gmail.com>
2022-08-03 19:37:43 +0500

    done

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,7 +2,7 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
-        double a = 2, b = 10, c;
-        c = ((9 * Math.pow(a, 2)) - (5 * b) + 2 + a - 7) * ((a + b - (4 * a * b)) / 2);
+        double a = 2, b = 10;
+        System.out.println(((9 * Math.pow(a, 2)) - (5 * b) + 2 + a - 7) * ((a + b - (4 * a * b)) / 2));
     }
 }
--- src/main/java/lang/print/gaps/task4/SettingTypes.java
+++ src/main/java/lang/print/gaps/task4/SettingTypes.java
@@ -1,20 +1,11 @@
 package lang.print.gaps.task4;
-
-//import org.slf4j.Logger;
-//import org.slf4j.LoggerFactory;
-
-
-import java.util.logging.Logger;
-
 public class SettingTypes {
-
-//    private static final Logger log = LoggerFactory.getLogger(SettingTypes.class);
-    private static final Logger loger = Logger.getLogger(SettingTypes.class.getName());
     public static void main(String[] args) {
 
             long first = 10_000_000_000L;
-            loger.fine(String.valueOf(first));
+            System.out.println(first);
             char second = 'a';
+            System.out.println(second);
             boolean third = 5 > 9;
             System.out.println(third);
             double forth = 12.90434323324342;
