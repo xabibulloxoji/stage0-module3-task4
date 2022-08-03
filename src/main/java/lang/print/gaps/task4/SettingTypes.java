679043f5b03f2ecfbf6caf27193059511e5922a8
Muhammadsodiq <muhammadsodiqxabibulloxoji@gmail.com>
2022-08-03 18:36:37 +0500

    done

--- src/main/java/lang/print/gaps/task4/SettingTypes.java
+++ src/main/java/lang/print/gaps/task4/SettingTypes.java
@@ -1,32 +1,33 @@
 package lang.print.gaps.task4;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
 
+
+import org.slf4j.ILoggerFactory;
+
+import java.util.logging.Logger;
 
 public class SettingTypes {
 
-    private static final Logger log = LoggerFactory.getLogger(SettingTypes.class);
+//    private static final Logger log = LoggerFactory.getLogger(SettingTypes.class);
+    private static final Logger loger = Logger.getLogger(SettingTypes.class.getName());
     public static void main(String[] args) {
 
-        try{
             long first = 10_000_000_000L;
-            System.out.println(first);
+            loger.fine(String.valueOf(first));
             char second = 'a';
-            System.out.println(second);
+            loger.fine(String.valueOf(second));
             boolean third = 5 > 9;
-            System.out.println(third);
+            loger.fine(String.valueOf(third));
             double forth = 12.90434323324342;
-            System.out.println(forth);
+            loger.fine(String.valueOf(forth));
             float fifth = 12.3F;
-            System.out.println(fifth);
+            loger.fine(String.valueOf(fifth));
             byte sixth = 1;
-            System.out.println(sixth);
+            loger.fine(String.valueOf(sixth));
             short seventh = 129;
-            System.out.println(seventh);
-        } catch (Exception e){
-            log.warn(e.getMessage());
-        }
+            loger.fine(String.valueOf(seventh));
 
 
     }
